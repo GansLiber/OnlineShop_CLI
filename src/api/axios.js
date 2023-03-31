414362b9bf46c06cb1c594909cdf5aa08d92675e
zazaz <zazaza993@mail.ru>
2023-03-31 19:43:48 +0700

    ничего не работает, нет реквеста на апи для получения текущего юзера...

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,12 @@
 import axios from 'axios'
+import {getItem} from '@/helpers/persistanceStorage'
 
 axios.defaults.baseURL = 'https://jurapro.bhuser.ru/api-shop'
 
+axios.interceptors.request.use(config => {
+  const token = getItem('accessToken')
+  const authorizationToken = token ? `Token ${token}` : ''
+  console.log('token: ', authorizationToken)
+  return config
+})
 export default axios
